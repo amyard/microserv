12749d2c505aa056f78514dab41db864b5a4f4a9
Maksym Turenko <maksym@bundledocs.com>
2023-04-01 22:49:31 +0100

    Basket.API - repo, controller, implementation

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Entities;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    { }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+    
+    public string UserName { get; set; }
+    public List<ShoppingCartItems> Items { get; set; } = new List<ShoppingCartItems>();
+
+    public decimal TotalCost
+    {
+        get
+        {
+            return Items.Sum(item => item.Price * item.Quantity);
+        }
+    }
+}
